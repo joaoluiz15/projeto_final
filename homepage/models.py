a574ab1b99b258a82a07324eebdf33fa9a92f15e
joaoluiz15 <jllopes.joaoluiz@gmail.com>
2023-12-10 20:29:30 -0300

    modelos e views atualizados
    não tá reconhecendo nem mapeando o app homepage

--- homepage/models.py
+++ homepage/models.py
@@ -3,17 +3,14 @@ from django.db import models
 # Create your models here.
 
 class Cliente(models.Model):
-    cpf = models.IntegerField(primary_key=True)
+    cpf = models.IntegerField(primary_key=True, default=0)
     nome = models.CharField(max_length=50)
 
 class Carro(models.Model):
-    id = models.AutoField(primary_key=True, default=0)
+    id = models.AutoField(primary_key=True)
     nome = models.CharField(max_length=20)
     ano_fabricacao = models.IntegerField()
     categoria = models.CharField(max_length=10)
-    nome_cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
-
-
-
-
+    estoque = models.IntegerField(default=0)
+    cpf_cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, null=True, blank=True)
 
--- homepage/views.py
+++ homepage/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from homepage import Cliente, Carro
 
 # Create your views here.
+
+def home(request):
+    print('Página inicial')
+    lista_carros = Carro.objects.all()
+    template = loader.get_template("index.html")
+
+    context = {
+        'lista_carros': lista_carros,
+    }
+
+    return HttpResponse(template.render(context=context))
